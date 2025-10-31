860ef16f3f1e00bd969b80e2d91fd001bb003a43
Èrik Lledó <65349896+lledoerik@users.noreply.github.com>
2025-10-31 05:26:16 +0100

    Fix host string formatting in app.run

--- app.py
+++ app.py
@@ -398,7 +398,7 @@ if __name__ == '__main__':
         print("="*70 + "\n")
         
         try:
-            app.run(debug=True, host='https://recomandor.hermes.cat, use_reloader=False)
+            app.run(debug=True, host='https://recomandor.hermes.cat', use_reloader=False)
         except (KeyboardInterrupt, SystemExit):
             # Aturar el scheduler quan es tanca l'app
             scheduler.shutdown()
